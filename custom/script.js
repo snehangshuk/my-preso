c2b4fc1a779cfbbe55b161fc49091181cc3bdfa7
Snehangshu Karmakar <snehangshu@gmail.com>
2021-06-18 12:06:37 +0530

    Adds consistent header and custom CSS

---
+++ custom/script.js
@@ -0,0 +1,18 @@
+<script>
+toggleMenu = () => {
+        let menu = Reveal.getPlugin('menu');
+        if (menu) menu.toggle();
+      };
+goNext = () => {
+        Reveal.next();
+      };
+goPrev = () => {
+        Reveal.prev();
+      };
+goDown = () => {
+        Reveal.down();
+      };
+goUp = () => {
+        Reveal.up();
+      };
+</script>
